fea6a991acf21e3ba20253fdb86137885c681652
VityaPain <vitayzablockyy58@gmail.com>
2022-09-27 22:58:23 +0300

    states

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,39 +1,68 @@
+import React from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
-
-    let classNames = "list-group-item d-flex justify-content-between"
-    if (increase) {
-        classNames += " increase";
+class EmployeesListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            promotion: false
+        }
     }
-    // можно было и так, но это не очень красиво
-    // <li className={(increase ? 'increase ':'') + "list-group-item d-flex justify-content-between"}>
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input 
-                type="text" 
-                name="" 
-                id="" 
-                className="list-group-item-input"
-                defaultValue={salary + '$'}/>
-            <div className="d-flex justife-content-center align-items-center">
-                <button 
-                    type="button"
-                    className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase             // две пары желтых скобок это коллбек, чтобы не писать "this.state." деструктуризируют; вторые желтые скобки чтобы не писать return
+        }))
+    }
 
-                <button 
-                    type="button"
-                    className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onPromotion = () => {
+        this.setState(({promotion}) => ({
+            promotion: !promotion            
+        }))
+    }
+    
+    render() {
+        const {name, salary} = this.props;
+        const {increase, promotion} = this.state;        
+
+        let classNames = "list-group-item d-flex justify-content-between"
+        if (increase) {
+            classNames += " increase";
+        }
+        if (promotion) {
+            classNames += ' like';
+        }
+        // можно было и так, но это не очень красиво
+        // <li className={(increase ? 'increase ':'') + "list-group-item d-flex justify-content-between"}>
+    
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.onPromotion}>{name}</span>
+                <input 
+                    type="text" 
+                    name="" 
+                    id="" 
+                    className="list-group-item-input"
+                    defaultValue={salary + '$'}/>
+                <div className="d-flex justife-content-center align-items-center">
+                    <button 
+                        type="button"
+                        className="btn-cookie btn-sm"
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button 
+                        type="button"
+                        className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
