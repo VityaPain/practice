e045e09cbc119dead4b868e8b8531a2b4924eea3
VityaPain <vitayzablockyy58@gmail.com>
2022-10-09 23:18:54 +0300

    search panel + simple filters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -20,6 +20,8 @@ class App extends Component {
                 {name: 'Gojo S.', salary: 3000, increase: false, rise: false, id: uuidv4()},
                 {name: 'Naruto U.', salary: 500, increase: false, rise: false, id: uuidv4()}
             ],
+            term: '',
+            filter: 'all'
         }
     }
 
@@ -91,20 +93,53 @@ class App extends Component {
     }
 
     // так как смысл работы сверху и райза одинаковый, то лучше объеденить код
-    onToggleRise = (id) => {
-        this.setState(({data}) => ({
-            data: data.map(item => {
-                if (item.id === id) {
-                    return {...item, rise: !item.rise}
-                }
-                return item; 
-            })
-        }))
+    // onToggleRise = (id) => {
+    //     this.setState(({data}) => ({
+    //         data: data.map(item => {
+    //             if (item.id === id) {
+    //                 return {...item, rise: !item.rise}
+    //             }
+    //             return item; 
+    //         })
+    //     }))
+    // }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});      // тоже самое что term: term
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000': 
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+    
+    onFilterSelect = (filter) => {
+        this.setState({filter});
     }
 
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase === true).length;
+        // const visibleData = this.searchEmp(data, term);     // отфильтрованный массив работников
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
 
         return (
             <div className="app">
@@ -113,12 +148,15 @@ class App extends Component {
                     increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,15 +1,33 @@
-// import React from "react";
+import {Component} from 'react'
+
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return(
-        <input 
-            type="text" 
-            name="" 
-            id="" 
-            className="form-control search-input"
-            placeholder="Найти сотрудника"/>
-    )
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        let term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render() {
+        return(
+            <input 
+                type="text" 
+                name="" 
+                id="" 
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}/>
+        )
+    }
 }
 
 // class SearchPanel extends React.Component {
